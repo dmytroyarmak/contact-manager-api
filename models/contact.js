35067742ff73dda252448408fdbf3863f4c5b174
Dmytro Yarmak <dmytroyarmak@gmail.com>
2015-02-23 23:47:19 +0200

    Simplest implementation of storing contacts per session

--- models/contact.js
+++ models/contact.js
@@ -1,91 +1,94 @@
 var _ = require('underscore');
 
-var contactStorage = {
-	'1': {
-		id: 1,
-		name : 'Terrence S. Hatfield',
-		tel: '651-603-1723',
-		email: 'TerrenceSHatfield@rhyta.com',
-		faceId: 1
-	},
-	'2': {
-		id: 2,
-		name : 'Chris M. Manning',
-		tel: '513-307-5859',
-		email: 'ChrisMManning@dayrep.com',
-		faceId: 2
-	},
-	'3': {
-		id: 3,
-		name : 'Ricky M. Digiacomo',
-		tel: '918-774-0199',
-		email: 'RickyMDigiacomo@teleworm.us',
-		faceId: 3
-	},
-	'4': {
-		id: 4,
-		name : 'Michael K. Bayne',
-		tel: '702-989-5145',
-		email: 'MichaelKBayne@rhyta.com',
-		faceId: 4
-	},
-	'5': {
-		id: 5,
-		name : 'John I. Wilson',
-		tel: '318-292-6700',
-		email: 'JohnIWilson@dayrep.com',
-		faceId: 5
-	},
-	'6': {
-		id: 6,
-		name : 'Rodolfo P. Robinett',
-		tel: '803-557-9815',
-		email: 'RodolfoPRobinett@jourrapide.com',
-		faceId: 6
-	}
-};
-
-var currentId = 6;
-
-function getNextId () {
-	currentId += 1;
-	return currentId;
+function getNextId (contactStorage) {
+	contactStorage.currentId += 1;
+	return contactStorage.currentId;
 }
 
 function Contact (attributes) {
 	_.extend(this, attributes);
 }
 
+Contact.createContactStorage = function createContactStorage () {
+	return {
+		currentId: 6,
+		contacts: {
+			'1': {
+				id: 1,
+				name : 'Terrence S. Hatfield',
+				tel: '651-603-1723',
+				email: 'TerrenceSHatfield@rhyta.com',
+				faceId: 1
+			},
+			'2': {
+				id: 2,
+				name : 'Chris M. Manning',
+				tel: '513-307-5859',
+				email: 'ChrisMManning@dayrep.com',
+				faceId: 2
+			},
+			'3': {
+				id: 3,
+				name : 'Ricky M. Digiacomo',
+				tel: '918-774-0199',
+				email: 'RickyMDigiacomo@teleworm.us',
+				faceId: 3
+			},
+			'4': {
+				id: 4,
+				name : 'Michael K. Bayne',
+				tel: '702-989-5145',
+				email: 'MichaelKBayne@rhyta.com',
+				faceId: 4
+			},
+			'5': {
+				id: 5,
+				name : 'John I. Wilson',
+				tel: '318-292-6700',
+				email: 'JohnIWilson@dayrep.com',
+				faceId: 5
+			},
+			'6': {
+				id: 6,
+				name : 'Rodolfo P. Robinett',
+				tel: '803-557-9815',
+				email: 'RodolfoPRobinett@jourrapide.com',
+				faceId: 6
+			}
+		}
+	};
+};
+
 function newContact (attributes) {
 	return new Contact(attributes);
 }
 
-Contact.getAllContacts = function getAllContacts () {
-	return _.map(contactStorage, newContact);
+Contact.getAllContacts = function getAllContacts (contactStorage) {
+	return _.map(contactStorage.contacts, newContact);
 }
 
-Contact.getContactById = function getContactById (id) {
-	return newContact(contactStorage[id]);
+Contact.getContactById = function getContactById (contactStorage, id) {
+	return newContact(contactStorage.contacts[id]);
 }
 
-Contact.updateContact = function (id, data) {
-	if (contactStorage[id]) {
-		contactStorage[id] = data;
+Contact.updateContact = function (contactStorage, id, data) {
+	if (contactStorage.contacts[id]) {
+		contactStorage.contacts[id] = data;
 		return Contact.getContactById(id);
 	} else {
 		throw new Error('There is no contact with id: ' + id);
 	}
 }
 
-Contact.createContact = function (data) {
-	var id = getNextId();
+Contact.createContact = function (contactStorage, data) {
+	var id = getNextId(contactStorage);
 	data.id = id;
-	contactStorage[id] = data;
+	contactStorage.contacts[id] = data;
 	return Contact.getContactById(id);
 }
 
-Contact.deleteContact = function (id) {
-	delete contactStorage[id];
+Contact.deleteContact = function (contactStorage, id) {
+	delete contactStorage.contacts[id];
 }
 
 exports.Contact = Contact;
--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 var express = require('express');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+var session = require('express-session');
 var Contact = require('./models/contact').Contact;
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use(function (req, res, next) {
+  if (!req.session.contactStorage) {
+    req.session.contactStorage = Contact.createContactStorage();
+  }
+
+  next();
+});
 
 app.use(bodyParser.json({type: '*/*'}));
 
@@ -14,30 +29,29 @@ app.use(function(req, res, next) {
 });
 
 app.get('/contacts', function (req, res) {
-	var contacts = Contact.getAllContacts();
+	var contacts = Contact.getAllContacts(req.session.contactStorage);
   	res.send(contacts);
-})
+});
 
 app.post('/contacts', function (req, res) {
-	var	contact = Contact.createContact(req.body);
+	var	contact = Contact.createContact(req.session.contactStorage, req.body);
   	res.send(contact);
-})
+});
 
 app.get('/contacts/:id', function (req, res) {
-	var	contact = Contact.getContactById(req.params.id);
+	var	contact = Contact.getContactById(req.session.contactStorage, req.params.id);
   	res.send(contact);
-})
+});
 
 app.put('/contacts/:id', function (req, res) {
-	var	contact = Contact.updateContact(req.params.id, req.body);
+	var	contact = Contact.updateContact(req.session.contactStorage, req.params.id, req.body);
   	res.send(contact);
-})
+});
 
 app.delete('/contacts/:id', function (req, res) {
-	Contact.deleteContact(req.params.id);
+	Contact.deleteContact(req.session.contactStorage, req.params.id);
   	res.send({});
-})
-
+});
 
 var server = app.listen(process.env.PORT || 3000, function () {
 
